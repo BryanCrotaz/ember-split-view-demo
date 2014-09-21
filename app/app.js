eff214438789d80d57645ccfa99c87cab6c623af
Bryan Hunt <bhunt@mac.com>
2014-09-21 09:27:20 -0500

    Updated to ember-split-view 0.4.0

--- app/app.js
+++ app/app.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  modulePrefix: 'ember-split-view-demo', // TODO: loaded via config
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
-loadInitializers(App, 'ember-split-view-demo');
+loadInitializers(App, config.modulePrefix);
 
 export default App;
